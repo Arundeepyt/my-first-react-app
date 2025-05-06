9fde928980b5bfdb0902e83ce8ecb7f52aa2ca2d
Arundeepyt <arunsodhi16@gmail.com>
2025-05-06 13:36:19 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="neon-text">Welcome to My Neon App</h1>
+      <button className="neon-button">Click Me</button>
+    </div>
+  );
+}
+
+export default App;
